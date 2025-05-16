3bb9fc4d7636706d747a65631123e97b84917ab4
Georgios Tselikas <georgios.p.tselikas@gmail.com>
2025-05-16 14:03:14 +0300

    product controller update

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -20,7 +20,7 @@ exports.findAll = async(req, res) => {
 }
 
 exports.findOneById = async(req, res) => {
-  console.log("Find product with specific id");
+  console.log("Find product with specific product ID");
   const id = req.params.id;
 
   try {
@@ -34,7 +34,59 @@ exports.findOneById = async(req, res) => {
     console.log("Problem in finding product", err)
     res.status(400).json({status: false, data: err});
   }
+}
 
+exports.create = async(req, res) => {
+  console.log("Create User");
+  let data = req.body;  
 
+  const newProduct = new Product({
+    product: data.product,
+    cost: data.cost,
+    description: data.description,
+    quantity: data.quantity
+  })
 
+  try{
+    const result = await newProduct.save();
+    res.status(200).json({status: true, data: result});
+  } catch (err) {
+    console.log("Problem in creating user", err);
+    res.status(400).json({status: false, data: err});
+  }
+}
+
+exports.updateById = async(req, res) => {
+  const id = req.body.id;
+
+  console.log("Update product with productId", id);
+
+  const updateProduct = {
+    product: req.body.product,
+    cost: req.body.cost,
+    description: req.body.description,  
+    quantity: req.body.quantity
+  };
+
+  try {
+    const result = await Product.findOneAndUpdate({_id: id}, updateProduct, {new: true});
+    res.status(200).json({status:true, data:result});
+  } catch(err){
+    console.log("Problem in updating product", err);
+    res.status(400).json({status:false, data:err});
+  }
+}
+
+exports.deleteById = async(req, res) => {
+  const id = req.body.id;
+
+  console.log("Delete product by id", id);
+
+  try {
+    const result = await Product.findOneAndDelete({_id: id});
+    res.status(200).json({status:true, data: result})
+  } catch(err) {
+    console.log("Problem in deleting by product ID", err);
+    res.status(400).json({status: false, data: err});
+  }
 }
